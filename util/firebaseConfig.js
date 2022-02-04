eb0692e3f4f10055944591820ac7042b5657574a
Cooper Saye <coopersaye@gmail.com>
2022-02-04 12:47:56 -0800

    Import firebaseConfig.

---
+++ util/firebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyATMu8TM0TvcFxAqpPzjt58U2eRdhwmTbw",
+  authDomain: "go-group-app.firebaseapp.com",
+  projectId: "go-group-app",
+  storageBucket: "go-group-app.appspot.com",
+  messagingSenderId: "603557677820",
+  appId: "1:603557677820:web:7e16e43f1a566aa052a579"
+};
