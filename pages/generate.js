39df8882583a037f5562b703c5d6e80407c6aa98
Cooper Saye <coopersaye@gmail.com>
2022-02-06 23:18:39 -0800

    Map persons.

--- pages/generate.js
+++ pages/generate.js
@@ -3,9 +3,21 @@ import Header from '../components/Header';
 import styles from '../styles/pages/Generate.module.css';
 
 export default function Generate() {
+  const [persons, setPersons] = useState(undefined);
+
   return (
     <div>
       <Header />
+      <div>
+        {
+          persons &&
+          persons.map((person, i) =>
+            <div key={i}>
+              <p>{person.name}</p>
+            </div>
+          )
+        }
+      </div>
     </div>
   );
 }
