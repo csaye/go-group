ed433278dcb97001fbc20a219ff966534f2c4db1
Cooper Saye <coopersaye@gmail.com>
2022-02-06 23:18:52 -0800

    Get persons from firebase.

--- pages/generate.js
+++ pages/generate.js
@@ -1,9 +1,26 @@
 import Header from '../components/Header';
 
+import { useEffect, useState } from 'react';
+import { getFirestore, getDocs, collection } from 'firebase/firestore';
+
 import styles from '../styles/pages/Generate.module.css';
 
 export default function Generate() {
   const [persons, setPersons] = useState(undefined);
+
+  const db = getFirestore();
+
+  // gets persons from firebase
+  async function getPersons() {
+    const personsRef = collection(db, 'persons');
+    const personsDocs = await getDocs(personsRef);
+    setPersons(personsDocs.docs.map(doc => doc.data()));
+  }
+
+  // get persons on start
+  useEffect(() => {
+    getPersons();
+  }, []);
 
   return (
     <div>
