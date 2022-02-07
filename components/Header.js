efc27e4585f6495059fdd36701299b4c0e5edd3d
Cooper Saye <coopersaye@gmail.com>
2022-02-06 22:27:14 -0800

    Add header content.

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,19 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
 import styles from '../styles/components/Header.module.css';
 
 export default function Header() {
   return (
     <div>
+      <Image src="/logo.png" width="48" height="48" />
+      <h1>GoGroup</h1>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+      <Link href="/">
+        <a>Generate</a>
+      </Link>
     </div>
   );
 }
