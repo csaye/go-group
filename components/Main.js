d42eac6ebd2e0643b0e22d8b50daf0cb21a21383
Cooper Saye <coopersaye@gmail.com>
2022-02-08 22:20:31 -0800

    Listen for user auth.

--- components/Main.js
+++ components/Main.js
@@ -1,10 +1,23 @@
 import { useState, useEffect } from 'react';
+import { getAuth } from 'firebase/auth';
+
 import styles from '../styles/components/Main.module.css';
 
 export default function Main(props) {
   const { Component, pageProps } = props;
 
   const [authed, setAuthed] = useState(false);
+
+  // initialize auth
+  const auth = getAuth();
+
+  // listen for user auth
+  useEffect(() => {
+    const authListener = auth.onAuthStateChanged(() => {
+      setAuthed(!!auth.currentUser);
+    });
+    return () => authListener;
+  }, []);
 
   return (
     <Component
